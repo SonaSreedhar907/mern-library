730c4c372339c270893723b18892f818daa8bd35
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-11-02 20:55:47 +0530

    create signin api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 // import userRoutes from './routes/user.route.js'
 
 dotenv.config();
@@ -17,10 +18,9 @@ const app = express()
 
 
 app.use(express.json())
-
-
+app.use(cookieParser());
 app.listen(3002, () => {
-    console.log('server is running on port 3000..')
+    console.log('server is running on port 3002..')
 })
 
 
