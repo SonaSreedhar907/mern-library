ae9759ab1b9e9ca8ee0a2099db71dffa3e6dc519
Sona Sreedhar <sonasreedhar44@gmail.com>
2024-11-03 12:57:22 +0530

    created post apis

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js'
+import postRoutes from './routes/post.route.js'
 
 dotenv.config();
 
@@ -26,6 +27,7 @@ app.listen(3002, () => {
 
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
+app.use('/api/post', postRoutes)
 
 
 app.use((err, req, res, next) => {
